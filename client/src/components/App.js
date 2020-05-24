6fe888d0caf78c1acc81fecb9d440cde61eed17f
Praveen Kumar <praveenscience@gmail.com>
2020-05-24 10:24:24 +0100

    Added header.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Header from "./Shared/Header";
 
 const App = () => {
-  return <div>Hello World!</div>;
+  return (
+    <div className="App">
+      <Header dark={true} className="Header">
+        Shopping Shortlist
+      </Header>
+    </div>
+  );
 };
 
 export default App;
